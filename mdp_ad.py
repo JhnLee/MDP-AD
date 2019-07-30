e34c1a1dcb0a2b3ecfc504e76750362953bcfe7e
JungHoon Lee <1620497@gmail.com>
2019-07-30 11:15:02 +0900

    Remove UNK key and Modify make_transition_matrix

--- mdp_ad.py
+++ mdp_ad.py
@@ -4,25 +4,32 @@ from collections import Counter, OrderedDict
 
 
 class MdpAD:
-    def __init__(self, transition_dim=2, input_data=None, threshold=None):
+    def __init__(self, window_size=2, input_data=None, threshold=None):
         '''
-        Each log key value is added by 3, and then assigned to each col(row) of Transition matrix 
+        Each log key value is added by 2, and then assigned to each col(row) of Transition matrix 
         --------------
         BOS key -> 0
-        EOS key -> 1
-        Unknown key -> 2
+        EOS key -> 1        
         
         (EXAMPLE) 2, 1, 3 ... -> 0, 5, 4, 6 ...
-        (EXAMPLE) 6, -1, 2 ... -> 0, 9, 2, 5 ...
-        '''
-        if type(transition_dim) is not int:
-            raise ValueError('transition dimension should be integer format')
-        if transition_dim <= 1:
-            raise ValueError('dimension should be bigger than 1')
+        (EXAMPLE) 6, 12, 2 ... -> 0, 9, 2, 5 ...
         
-        self.dim = transition_dim
+        Params
+        --------------
+        window_size : Window size of Log key sequence.
+            This would be the number of elements joint probabilties contain
+        
+        threshold : If True, predict function only return sequences
+            whose novelty score is upper than the threshold
+        '''
+        if type(window_size) is not int:
+            raise ValueError('window_size should be integer format')
+        if window_size <= 1:
+            raise ValueError('window_size should be bigger than 1')
+        
+        self.dim = window_size
         self.threshold = threshold
-        self.BOS_key, self.EOS_key, self.UNK_key = 0, 1, 2
+        self.BOS_key, self.EOS_key = 0, 1
         self.training_data = None
         
         if input_data is not None:
@@ -59,41 +66,37 @@ class MdpAD:
         anomaly = {k: s for k, s in score if s > self.threshold}
         return anomaly
 
+    def count_train_log(self):
+        if self.training_data:
+            count = Counter()
+            for d in self.training_data.values():
+                count.update(d)
+            return dict(sorted(count.items(), key=lambda x: x[0]))
+        else:
+            raise ValueError('No training data exist')
+    
     def _make_transition_matrix(self):
         '''Make transition matrix from whole log sequences'''
-        count = Counter()
-        for d in self.training_data.values():
-            count.update(d)
+        # prob = P(X == x_i | x_i-1, x_i-2, ...)
+        # --> denominator = P(x_i-1, x_i-2, ...)
+        # --> numerator = P(x_i, x_i-1, x_i-2, ...)
         
-        # Add idx of UNK token
-        count[2] = 1
-        
-        # Sort counter by key
-        count = dict(sorted(count.items(), key=lambda x: x[0]))
-        n_log = list(count.keys())[-1] + 1
-        
-        trans_mat = np.zeros(tuple([n_log] * self.dim))
+        # size of transition matrix : (max log key value + 3)^(dim)        
+        freq_mat = np.zeros(tuple([self._maximum_key + 3] * self.dim))
         window_sequences = [self._window(x) for x in list(self.training_data.values())]
 
         # Update frequency on the transition matrix
         for seq in window_sequences:
             for window in seq:
-                trans_mat[window] += 1
+                freq_mat[window] += 1
 
-        # Make frequency to prob
-        log_count = np.array(list(count.values()))[:, np.newaxis]
-        if self.dim > 2:
-            # prob = P(X == x_i | x_i-1, x_i-2, ...)
-            # --> log count = P(x_i-1, x_i-2, ...)
-            log_count = np.expand_dims(np.sum(trans_mat, axis=self.dim-1), axis=self.dim-1)
-            # to avoid error about dividing zero
-            log_count = np.where(log_count > 0, log_count, 1)
-        
-        self.transition_matrix = trans_mat / log_count       
-        
-        count[2] = 0
-        self.training_data_count = count
-        
+        # Make frequency to prob      
+        denom = np.expand_dims(np.sum(freq_mat, axis=self.dim-1), axis=self.dim-1)
+        # Temprarily assign value 1 to avoid Devide by Zero error
+        # [0, 0, 0, 0] / 0 => [0, 0, 0, 0] / 1
+        denom = np.where(denom>0, denom, 1)
+        self.transition_matrix = np.divide(freq_mat, denom)    
+
         # Decode the log sequences
         self.training_data = self._decode(self.training_data)
 
@@ -122,16 +125,24 @@ class MdpAD:
             yield result
             
     def _encode(self, data):
-        '''Add 3 to every element in the sequence'''
-        data = OrderedDict([(b, [self.BOS_key] + [e + 3 for e in s] + [self.EOS_key])
-                          for b, s in data.items()])
-        return data
+        '''Add 2 to every element in the sequence and track maximum key value'''
+        encoded_data = OrderedDict()
+        maximum_key = -1
+        
+        for b, s in data.items():
+            max_tmp = max(s)
+            maximum_key = max_tmp if max_tmp > maximum_key else maximum_key
+            encoded_data[b] = [self.BOS_key] + [e + 2 for e in s] + [self.EOS_key]
+        
+        self._maximum_key = maximum_key
+        
+        return encoded_data
     
     def _decode(self, data):
         '''Subtract 3 from every key and delete EOS and BOS tokens'''
         keys = list(data.keys())
         vals = list(data.values())
         vals = [val[1:-1] for val in vals]
-        data = OrderedDict([(key, [e - 3 for e in val]) 
+        data = OrderedDict([(key, [e - 2 for e in val]) 
                             for key, val in zip(keys, vals)])
         return data
