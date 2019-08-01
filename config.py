db198372420ec2e34492de0f3ecf6fe9bf2cf805
JungHoon Lee <1620497@gmail.com>
2019-08-01 22:22:57 +0900

    Add Config and Modify code

---
+++ config.py
@@ -0,0 +1,11 @@
+# Configuration File
+
+TRAIN_DATA_PATH = './Drain/baseline/blk_logkey_seq.pkl'
+TEST_DATA_PATH = './Drain/baseline/test_blk_logkey_seq.pkl'
+LABEL_PATH = './HDFS_split/anomaly_label.csv'
+
+OUTPUT_FILE_PATH = './output/result.csv'
+TRANSITION_MATRIX_PATH = './output/transition_matrix.pkl'
+
+WINDOW_SIZE = 2
+THRESHOLD = None
--- mdp_ad.py
+++ mdp_ad.py
@@ -1,4 +1,7 @@
 import numpy as np
+import pandas as pd
+import config
+import pickle
 from itertools import islice
 from collections import Counter, OrderedDict
 
@@ -23,7 +26,7 @@ class MdpAD:
             whose novelty score is upper than the threshold
         '''
         if type(window_size) is not int:
-            raise ValueError('window_size should be integer format')
+            raise ValueError('window_size should be integer format, your file format is {}'.format(type(window_size)))
         if window_size <= 1:
             raise ValueError('window_size should be bigger than 1')
         
@@ -43,7 +46,7 @@ class MdpAD:
             if self.training_data is None:
                 raise ValueError('No input data')
             else: 
-                self.training_data =self._encode(input_data)
+                self.training_data =self._encode(self.training_data)
         else:
             if self.training_data is not None:
                 raise ValueError('Got 2 input data')
@@ -56,9 +59,19 @@ class MdpAD:
         '''Predict novelty score from test dataset'''
         if type(test_data) is not dict:
             raise ValueError('Input data is not in a valid format (Should be a dictionary)')
-
-        test_data = self._encode(test_data)
-        score = {k: self._get_novelty_score(s) for k, s in test_data.items()}
+        
+        # Exclude unseen log key which is assigned as -1 by Drain parser
+        self.test_data = {k: v for k, v in test_data.items() if not -1 in v}
+        unseen_num = np.sum([-1 in v for v in list(test_data.values())])
+        if unseen_num > 0:
+            print("Number of unseen log key during prediction: {}".format(unseen_num))
+            print("Sequences having unseen log key are excluded")
+        
+        self.test_data = self._encode(self.test_data)
+        score = {k: np.sum(self.get_seq_score(s)) / (len(s) - (self.dim-1)) 
+                 for k, s in self.test_data.items()}
+        self.test_data = self._decode(self.test_data)
+        
         if self.threshold is None:
             return score
         
@@ -95,23 +108,26 @@ class MdpAD:
         # Temprarily assign value 1 to avoid Devide by Zero error
         # [0, 0, 0, 0] / 0 => [0, 0, 0, 0] / 1
         denom = np.where(denom>0, denom, 1)
-        self.transition_matrix = np.divide(freq_mat, denom)    
+        self._transition_matrix = np.divide(freq_mat, denom)    
 
         # Decode the log sequences
         self.training_data = self._decode(self.training_data)
-
-    def _get_novelty_score(self, seq):
-        '''Calculate expectation of transition prob in the log sequence'''
+    
+    @property
+    def transition_matrix(self):
+        return self._transition_matrix
+    
+    def get_seq_score(self, seq):
+        '''Calculate transition prob of each window in the log sequence'''
         if type(seq) is not list:
             raise ValueError('Input data should be in a list format')
         windows = list(self._window(seq))
-
+        
         def get_neg_log(x):
-            prob = self.transition_matrix[x]
+            prob = self._transition_matrix[x]
             return -np.log(1e-9 if prob == 0 else prob)
-
-        neg_log_sum = np.sum(list(map(get_neg_log, windows)))
-        return neg_log_sum / (len(seq) - (self.dim-1))
+        
+        return list(map(get_neg_log, windows))
 
     def _window(self, seq):
         '''Make sequence of transition from log sequence'''
@@ -139,10 +155,49 @@ class MdpAD:
         return encoded_data
     
     def _decode(self, data):
-        '''Subtract 3 from every key and delete EOS and BOS tokens'''
+        '''Subtract 2 from every key and delete EOS and BOS tokens'''
         keys = list(data.keys())
         vals = list(data.values())
         vals = [val[1:-1] for val in vals]
         data = OrderedDict([(key, [e - 2 for e in val]) 
                             for key, val in zip(keys, vals)])
+
         return data
+
+    
+def main():
+    with open(config.TRAIN_DATA_PATH, 'rb') as f:
+        trn = pickle.load(f)
+    with open(config.TEST_DATA_PATH, 'rb') as f:
+        tst = pickle.load(f)
+    
+    anomaly_label = pd.read_csv(config.LABEL_PATH)
+    id2label = dict(zip(anomaly_label['BlockId'], anomaly_label['Label']))
+    
+    print("Fitting on Training Dataset...")
+    mdp = MdpAD(config.WINDOW_SIZE, trn, config.THRESHOLD)
+    mdp.fit()
+    print("Done.")
+    
+    # Save transition Matrix
+    with open(config.TRANSITION_MATRIX_PATH, 'wb') as f:
+        pickle.dump(mdp.transition_matrix, f)
+    print('Saved Transition Matrix')
+    
+    mdp_pred = mdp.predict(tst)
+    mdp_pred_score = list(mdp_pred.values())
+    
+    print("Saving result file...")
+    result = pd.DataFrame({'BlockID': list(mdp.test_data.keys()),
+                          'log_sequence': list(mdp.test_data.values()),
+                          'window_neg_log_score': [mdp.get_seq_score(s) for s in list(mdp._encode(mdp.test_data).values())],
+                          'novelty_score': mdp_pred_score,
+                          'label': [id2label[b] for b in list(mdp.test_data.keys())]})
+    # Save Result file
+    with open(config.OUTPUT_FILE_PATH, 'wb') as f:
+        pickle.dump(result, f)
+    print("Done.")
+    
+if __name__ == '__main__':
+    
+    main()
